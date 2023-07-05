e1a2f12d0b0987fcd3d9b5b035bda6f4a0521a28
nishan akalanka <hdnakalanka@gmail.com>
2023-07-05 12:53:14 +0530

    initializing book-service

---
+++ back-end/book-service/src/test/java/lk/ijse/dep10/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
