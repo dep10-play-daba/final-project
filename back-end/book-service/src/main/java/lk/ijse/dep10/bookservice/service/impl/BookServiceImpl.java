9b9389e11c4c1a3e45264c7532f5b25d85cd9bb7
nishan akalanka <hdnakalanka@gmail.com>
2023-07-05 14:22:35 +0530

    setting ModelMapper

--- back-end/book-service/src/main/java/lk/ijse/dep10/bookservice/service/impl/BookServiceImpl.java
+++ back-end/book-service/src/main/java/lk/ijse/dep10/bookservice/service/impl/BookServiceImpl.java
@@ -1,4 +1,40 @@
 package lk.ijse.dep10.bookservice.service.impl;
 
-public class BookServiceImpl {
+import lk.ijse.dep10.bookservice.dto.BookDTO;
+import lk.ijse.dep10.bookservice.repository.BookRepository;
+import lk.ijse.dep10.bookservice.service.BookService;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void saveBook(BookDTO book) {
+
+    }
+
+    @Override
+    public void updateBook(BookDTO book) {
+
+    }
+
+    @Override
+    public void deleteBook(String isbn) {
+
+    }
+
+    @Override
+    public void getBook(String isbn) {
+
+    }
+
+    @Override
+    public List<BookDTO> findBooks(String query) {
+        return null;
+    }
 }
